2926ef3ac01c813cc9a45211997557abe9282068
KaiserPhemi <kaiser.phemi@protonmail.ch>
2023-02-07 20:36:31 +0100

    feature()
    - add server

--- main.go
+++ main.go
@@ -13,22 +13,39 @@ func main() {
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/home", homeHandler)
-	
+
 	fmt.Printf("Starting server on port 7000\n")
-if err := http.ListenAndServe(":7000", nil); err != nil {
-	log.Fatal(err)
-}
+	if err := http.ListenAndServe(":7000", nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
 // Handles form route
-func formHandler(w http.ResponseWriter, r *http.Request){
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Parseform error: %v", err)
+		return
+	}
 
+	fmt.Fprintf(w, "Request Successfully parsed")
+	name := r.FormValue("name")
+
+	fmt.Print(name)
 }
 
-
 // handles main route
-func homeHandler(w http.ResponseWriter, r *http.Request){
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	// checks for route
 	if r.URL.Path != "/home" {
 		http.Error(w, "Invalid URL", http.StatusBadRequest)
+		return
 	}
+
+	// checks if method is not GET
+	if r.Method != "GET" {
+		http.Error(w, "Invalid method", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "Welcome Home")
 }
